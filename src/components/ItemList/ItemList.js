6940d536ffca25ad06cb9043a1c46c0315bc606e
milagrosabigail23 <milagrosabigailbelarmino@gmail.com>
2023-05-08 15:56:46 -0300

    agrego carpeta Itemlist

---
+++ src/components/ItemList/ItemList.js
@@ -0,0 +1,10 @@
+const ItemList = ({products}) => {
+    return(
+        <div className = 'ListGroup'>
+            {products.map(prod => <Item key={prod.id} {...prod} />)}
+        </div>
+    )
+}
+
+
+export default ItemList
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,28 @@
+import { useState , useEffect } from 'react'
+import { getProducts } from '../ItemList/ItemList'
+
 const ItemListContainer = ({ greeting }) => {
+    const [products, setProducts] = useState([])
+
+
+    useEffect (() => {
+        getProducts()
+            .then(response => {
+            setProducts(response)
+            })
+            .catch(error=>{
+                console.error(error)
+        })
+    }, [])
+
+
     return (
         <div>
-            <h1>{greeting}</h1>
+            <h1> {greeting} </h1>
+            <ItemList products = {products}/>
         </div>
     )
 }
+
 
 export default ItemListContainer
