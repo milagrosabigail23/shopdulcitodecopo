c0842a48abd66166296b18e9773003e1ec24f567
milagrosabigail23 <milagrosabigailbelarmino@gmail.com>
2023-05-08 15:38:56 -0300

    agrego carpeta ItemCount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
 import './App.css';
-import ItemListContainer from './components/NavBar/ItemListContainer/ItemListContainer';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComp from './components/NavBar/NavBar';
+import ItemCount from './components/ItemCount/ItemCount';
+
 
 function App() {
   return (
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React , {Component} from "react"
 import {Navbar,Nav,NavDropdown,Container} from 'react-bootstrap'
-import CartWidget from "./CartWidget/CartWidget"
+import CartWidget from "../CartWidget/CartWidget"
 
 export default class NavbarComp extends Component {
     render() {
